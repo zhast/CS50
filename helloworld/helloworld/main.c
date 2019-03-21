6c628cd89d16d1b1b4c372400596f20ae9f9108a
Steven <stevezzhang@gmail.com>
2019-03-21 11:33:57 -0400

    First commit

---
+++ helloworld/helloworld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  helloworld
+//
+//  Created by Steven Zhang on 2019-01-25.
+//  Copyright © 2019 Steven Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
